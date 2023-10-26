b96f1aeaa257831fd8bcea9e73a098b84ceb8a93
Armand Araujo <armandaraujo@Armands-MacBook-Pro.local>
2023-10-25 22:05:53 -0700

    added 2nd round of initial code, running further tests

--- server.js
+++ server.js
@@ -2,28 +2,56 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path')
+const db = ('./db/db.json')
 
-const currentTasks = require('./db/db.json');
+// const currentTasks = require('./db/db.json');
 const PORT = 3001;
-
-
-app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-
-)
-
-app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/notes.html')));
-
-app.delete(    (req, res)=> {
-
-const something = (req.params.something)});
 
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) => 
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+res.json(db)
+});
+
+
+
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = data.length + 1;
+    data.push(newNote);
+
+    // stringify data?
+
+    fs.writeFile('db.json', JSON.stringify(data, null, 2), (err) => {
+
+        if(err) {
+            console.error('Error writing to db.json:', err);
+            return;
+        }
+        res.json(db)
+    })
+})
+
+// app.delete(    (req, res)=> {
+
+// const something = (req.params.something)});
+
+
+
 
 
 
